0cbcbd0475de165d5127d0bc2e80d9981393574c
Tom <git@thomastrapp.com>
2021-12-20 21:21:16 +0100

    switch from travis ci to github actions

--- scripts/github-actions/build.sh
+++ scripts/github-actions/build.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
-
-REPOS_DIR=$(readlink -f .)
 
 sudo apt-get update -yy
 sudo apt-get install -yy \
-  wget git python2.7 python build-essential libxml2 \
+  wget python build-essential libxml2 \
   libtool autoconf rapidjson-dev cmake
 
 # setup emsdk
-cd ~
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 git pull
 ./emsdk install latest
 ./emsdk activate latest
 source ./emsdk_env.sh
+cd ..
 
 # setup hext-emscripten
-cd "$REPOS_DIR"
 make
 make test
-readlink -f hext-emscripten.*
 
